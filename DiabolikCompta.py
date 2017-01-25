4dd9cc5236210fa31c645194f64ce7f45818f6a2
hoccau <hoccau@linuxmail.org>
2017-01-25 10:35:40 +0100

    fix bug about new db creation

--- DiabolikCompta.py
+++ DiabolikCompta.py
@@ -31,7 +31,6 @@ class MainWindow(QMainWindow):
         exitAction = self.add_action('&Quitter', qApp.quit, 'Ctrl+Q')
         newAction = self.add_action('Nouveau', self.create_new_db, 'Ctrl+N')
         openAction = self.add_action('&Ouvrir', self.open_db, 'Ctrl+O')
-        closeAction = self.add_action('&Fermer', self.close_db, 'Ctrl+W')
         aboutAction = self.add_action('à propos', self.about_d)
 
         self.db_actions = {}
@@ -45,12 +44,13 @@ class MainWindow(QMainWindow):
         self.db_actions['ViewRapport'] = self.add_action('Rapport', self.viewRapport)
         self.db_actions['exportPdf'] = self.add_action('Exporter un rapport PDF', self.export_pdf)
         self.db_actions['exportXlsx'] = self.add_action('Exporter un fichier Excel', self.export_excel)
+        self.db_actions['close'] = self.add_action('&Fermer', self.close_db, 'Ctrl+W')
         
 
         fileMenu = menubar.addMenu('&Fichier')
         fileMenu.addAction(newAction)
         fileMenu.addAction(openAction)
-        fileMenu.addAction(closeAction)
+        fileMenu.addAction(self.db_actions['close'])
         fileMenu.addAction(self.db_actions['exportPdf'])
         fileMenu.addAction(self.db_actions['exportXlsx'])
         fileMenu.addAction(exitAction)
@@ -171,17 +171,18 @@ class MainWindow(QMainWindow):
                 "Enregistrer une nouvelle base",
                 None,
                 'DB(*.db)')
-        if db_name.split('.')[-1] != 'db':
-            db_name += '.db'
-        progress_dialog = WaitingDialog(
-            self,
-            self.model,
-            db_name,
-            code)
-        progress_dialog.resize(200, 100)
-        if progress_dialog.exec_():
-            self.connect_db(db_name)
-            self.set_infos()
+            if db_name:
+                if db_name.split('.')[-1] != 'db':
+                    db_name += '.db'
+                progress_dialog = WaitingDialog(
+                    self,
+                    self.model,
+                    db_name,
+                    code)
+                progress_dialog.resize(200, 90)
+                if progress_dialog.exec_():
+                    self.connect_db(db_name)
+                    self.set_infos()
 
     def connect_db(self, db_path):
         self.model.connect_db(db_path)
@@ -217,7 +218,6 @@ class MainWindow(QMainWindow):
         code, ok = QInputDialog.getInt(self, 'Identifiant de la pièce',
             'Entrez l\'identifiant de la pièce à éditer')
         if ok:
-            print(self.model.get_(['id'], 'pieces_comptables'))
             ids_list = self.model.get_(['id'], 'pieces_comptables')
             ids = [item for l in ids_list for item in l]
             if code in ids:
@@ -225,7 +225,6 @@ class MainWindow(QMainWindow):
                 self.piece_comptable.show()
             else:
                 QMessageBox.warning(self, "Erreur", "Cette pièce n'existe pas.")
-                
 
     def add_piece_comptable(self):
         self.piece_comptable = PieceComptable(self)
@@ -267,9 +266,11 @@ class Thread(QThread):
         self.finished.emit()
 
 app = QApplication(sys.argv)
-#translator = QTranslator()
-#translator.load('qt_' + QLocale.system().name(), 
-#    QLibraryInfo.location(QLibraryInfo.TranslationsPath))
-#app.installTranslator(translator)
+translator = QTranslator()
+print(QLocale.system().name())
+print(QLibraryInfo.TranslationsPath)
+translator.load('qt_' + QLocale.system().name(), 
+    QLibraryInfo.location(QLibraryInfo.TranslationsPath))
+app.installTranslator(translator)
 main_window = MainWindow()
 sys.exit(app.exec_())
